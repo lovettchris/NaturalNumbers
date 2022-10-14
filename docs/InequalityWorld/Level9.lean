348247bc963f5ab61395cba320f9933aa8e8c809
Chris Lovett <clovett@microsoft.com>
2022-10-14 04:30:02 -0700

    publish

--- docs/InequalityWorld/Level9.lean
+++ docs/InequalityWorld/Level9.lean
@@ -28,7 +28,8 @@ theorem le_total (a b : MyNat) : a ≤ b ∨ b ≤ a := by
       left
       exact zero_le _
     | succ a =>
-      cases (hd a) with
+      have h2 := hd a
+      cases h2 with
       | inl h =>
         left
         exact succ_le_succ a d h
@@ -38,6 +39,13 @@ theorem le_total (a b : MyNat) : a ≤ b ∨ b ≤ a := by
 
 /-!
 See the [revert tactic](../Tactics/revert.lean.md)
+
+Note in the above proof that `exact succ_le_succ a d h`
+is just shorthand for:
+`
+apply succ_le_succ a d
+exact h
+`
 
 Another collectible: the naturals are a linear order.
 
